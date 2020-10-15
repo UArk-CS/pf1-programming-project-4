e19493c4bb3fa2b70388e0b8ecbf01c5b6622ccb
Benjamin Worthington <benworthin@icloud.com>
2020-10-14 19:50:42 -0500

    Fixed whitespace and style

--- main.cpp
+++ main.cpp
@@ -12,52 +12,67 @@ const int MAX_DL_HEALTH = 50;
 
 // Stun: reduce the opponent's speed to 0.
 bool Stun(const int SpeedDif) {
+
     double prob = (double) (SpeedDif + 1) / MAX_SPEED;
     double dice = (double) rand() / RAND_MAX;
-    if (dice < prob)
+
+    if (dice < prob) {
         return true;
-    else
+    } else {
         return false;
+    }
+
 }
 
 // Evade: avoid being hitting by the opponent's attack.
 bool Evade(const int SpeedDif) {
+
     double prob = (double) (SpeedDif + 1) / (MAX_SPEED + 2);
     double dice = (double) rand() / RAND_MAX;
-    if (dice < prob)
+
+    if (dice < prob) {
         return true;
-    else
+    } else {
         return false;
+    }
+
 }
 
 // ---------- Unforgivable Curses ----------
 
 // Avada Kedavra: Cause large health damage (health-10). Can be evaded if the player's speed is higher than the dark lord's speed.
-void AvadaKedavra(int & PlayerHealth, int & PlayerSpeed,
-    const int DLHealth,
-        const int DLSpeed) {
+void AvadaKedavra(int & PlayerHealth, int & PlayerSpeed, const int DLHealth, const int DLSpeed) {
+
     cout << "Dark Lord casts Avada Kedavra." << endl;
     int SpeedDif = PlayerSpeed - DLSpeed;
+
     if (Evade(SpeedDif) == false) {
+
         cout << "You are hit. Health-10." << endl;
         PlayerHealth -= 10;
+
     } else {
         cout << "You evaded it." << endl;
     }
+
 }
 
 // Imperio: Has chance of stun if the dark lord's speed is higher than the player's speed. 
-void Imperio(int & PlayerHealth, int & PlayerSpeed,
-    const int DLHealth,
-        const int DLSpeed) {
+void Imperio(int & PlayerHealth, int & PlayerSpeed, const int DLHealth, const int DLSpeed) {
+
     cout << "Dark Lord casts Imperio." << endl;
+
     int SpeedDif = DLSpeed - PlayerSpeed;
+
     if (Stun(SpeedDif) == true) {
+
         cout << "You are stunned." << endl;
         PlayerSpeed = 0;
+
     } else {
         cout << "It takes no effect on you." << endl;
     }
+
 }
 
 // Crucio: Cause medium damage and slow the player (health-5, speed-1). The player's speed cannot be reduced to 0 (minimum 1). 
@@ -65,11 +80,18 @@ void Imperio(int & PlayerHealth, int & PlayerSpeed,
 void Crucio(int &PlayerHealth, int &PlayerSpeed, const int DLHealth, const int DLSpeed) {
 
     cout << "Dark Lord casts Crucio." << endl;
+
     int SpeedDif = PlayerSpeed - DLSpeed;
+
     if (Evade(SpeedDif) == false) {
+
         cout << "You are hit. Health-5. Speed-1." << endl;
         PlayerHealth -= 5;
-        if (PlayerSpeed>1) PlayerSpeed -= 1;
+
+        if (PlayerSpeed>1) {
+            PlayerSpeed -= 1;
+        }
+
     } else {
         cout << "You evaded it." << endl;
     }
@@ -78,10 +100,10 @@ void Crucio(int &PlayerHealth, int &PlayerSpeed, const int DLHealth, const int D
 
 // ---------- Dark Lord Move ----------
 
-void DLMove(int & PlayerHealth, int & PlayerSpeed,
-    const int DLHealth,
-        const int DLSpeed) {
+void DLMove(int & PlayerHealth, int & PlayerSpeed, const int DLHealth, const int DLSpeed) {
+
     int choice = rand() % 3;
+
     switch (choice) {
     case 0:
         AvadaKedavra(PlayerHealth, PlayerSpeed, DLHealth, DLSpeed);
@@ -95,6 +117,7 @@ void DLMove(int & PlayerHealth, int & PlayerSpeed,
     default:
         ;
     }
+
 }
 
 // ---------- Hogwarts Spells ----------
@@ -103,10 +126,14 @@ void DLMove(int & PlayerHealth, int & PlayerSpeed,
 void Expelliarmus(const int PlayerHealth, const int PlayerSpeed, int & DLHealth, int & DLSpeed) {
 
     cout << "You cast Expelliarmus." << endl;
+
     int speedDif = PlayerSpeed - DLSpeed;
+
     if (Stun(speedDif) == true) {
+
         cout << "Dark Lord is stunned." << endl;
         DLSpeed = 0;
+
     } else {
         cout << "Dark Lord is not affected by your puny attack." << endl;
     }
@@ -120,10 +147,14 @@ void Expelliarmus(const int PlayerHealth, const int PlayerSpeed, int & DLHealth,
 void Scourgify(const int PlayerHealth, const int PlayerSpeed, int &DLHealth, int &DLSpeed) {
 
     cout << "You cast Scourgify with a smirk." << endl;
+
     int speedDif = DLSpeed - PlayerSpeed;
+
     if (!Evade(speedDif)) {
+
         cout << "You got him! Health: -15" << endl;
         DLHealth -= 15;
+
     } else {
         cout << "He evaded it... c'mon man." << endl;
     }
@@ -142,30 +173,28 @@ void Pack(const int PlayerHealth, const int PlayerSpeed, int &DLHeath, int &DLSp
     if (!Evade(speedDif)) {
 
         cout << "Ouch! That is going to leave a mark. Health -5. Speed -2." << endl;
-
         DLHeath -= 5;
+
         if (DLSpeed > 1) {
             DLSpeed -= 2;
         }
 
     } else {
-
         cout << "How does a big trunk miss? Get it together!" << endl;
-
     }
 
 }
 
 // ---------- Player Move ----------
 
-void PlayerMove(const int PlayerHealth,
-    const int PlayerSpeed, int & DLHealth, int & DLSpeed) {
+void PlayerMove(const int PlayerHealth, const int PlayerSpeed, int & DLHealth, int & DLSpeed) {
     // TODO: Implement the player's move in each round.  
 }
 
 // ---------- Main Function ----------
 
 int main() {
+
     srand(time(NULL));
 
     int PlayerHealth = MAX_PLAYER_HEALTH;
@@ -196,11 +225,13 @@ int main() {
 		round++;
     }
 
-    if (PlayerHealth > 0)
+    if (PlayerHealth > 0) {
         cout << "You win!" << endl;
-    else
+    } else {
         cout << "Dark lord wins." << endl;
+    }
 
     return 0;
+
 }
 
